1f3ecf41b8ce6a77d983c2fb37775d47f7d52dae
mplichta <bigcatplichta@gmail.com>
2019-10-05 13:27:01 -0600

    "added list of toys, updated Toy object to instantiate from hash"

--- lib/toy-class.rb
+++ lib/toy-class.rb
@@ -3,13 +3,16 @@ class Toy
     @@all = []
     @@donated_toys = []
 
-    def initialize(name= "Boomer Ball", price= "$30", description= "A nearly indestructible plastic ball. This one is a foot in diameter, great for all sorts of animals!")
+    def initialize(toy_hash)
         @status = "Waiting to be donated"
-        @@all << self 
-        @name = name
-        @price = price 
-        @description = description
+        toy_hash.each {|key, value| self.send("#{key}=", value)}
+        @@all << self
+        binding.pry
     end 
+
+    def self.create_from_collection(collection)
+        collection.each {|toy_hash| Toy.new(toy_hash)}
+    end
 
     def self.all
         @@all
@@ -46,3 +49,50 @@ class Toy
         
     end 
 end 
+
+# below collection creates a list of toys
+toy_list = [
+    {name: "Boomer Ball",
+    price: "$30",
+    description: "A nearly indestructible plastic ball. This one is a foot in diameter, great for all sorts of animals!"
+    },
+    {name: "Bungee Toy",
+    price: "$20",
+    description: "A durable stretchy firehose. Attach it to a fixture for a game of tug of war."
+    },
+    {name: "Hammock",
+    price: "$80",
+    description: "A hammock made of interwoven straps. Lounge around while eating bamboo shoots!"
+    },
+    {name: "Firehose Cube",
+    price: "$10",
+    description: "Recycled firehose woven into a dense, heavy cube. Good for strong animals."
+    },
+    {name: "Floaty Toy",
+    price: "$25",
+    description: "Buoyant toy, great for marine animals or anyone who likes to play in the pool."
+    },
+    {name: "Treat Dispenser",
+    price: "$30",
+    description: "This rolls around, dropping delicious treats as it goes."
+    },
+    {name: "Tube",
+    price: "$10",
+    description: "It's a tube. Small animals can crawl inside it. Large animals can stick their head in it. Tuuuuuuuuubbbbeeee!!!"
+    },
+    {name: "Puzzle Box",
+    price: "$40",
+    description: "Kind of like those toddler toys. Put the square peg into the square hole. Good for monkeys."
+    },
+    {name: "Rope swing",
+    price: "$20",
+    description: "Attach this to the ceiling of an enclosure. Animals can swing on it or grab it and pull it."
+    },
+    {name: "Hidey-hole",
+    price: "$15",
+    description: "Small box with a hole, kind of like a fort."
+    },
+    {name: "Crinkly Bag",
+    price: "$30",
+    description: "Durable bag that makes noises when scrunching it, great for cats."
+    }]
